ca8fbce4c31fa7f962c833cf48e94559014a7136
Fernando <f.olguin.ch@gmail.com>
2017-05-08 04:20:55 +0100

    Add edition and append to notes

--- notes.sh
+++ notes.sh
@@ -1,13 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 # A simple script to manage notes in the command line
 
+# Fixed variables
+NOTES=~/Notes
+EDITOR=vim
+
+# Functions
 function list_dir {
     # List the notes in the notes directory
     ls -lh $NOTES
 }
 
-NOTES=~/Notes
 
-if [ $1 = "-l" ]; then
-    list_dir
-fi
+#Main
+while [ "$1" != "" ]; do
+    case $1 in
+        -l | --list )   list_dir
+                        ;;
+        -e | --edit )   shift
+                        $EDITOR "$NOTES/$1"
+                        ;;
+        -a | --append ) shift
+                        message=$1
+                        echo $message >> "$NOTES/$1"
+                        ;;
+        * )             exit 1
+                        ;;
+    esac
+    shift
+done
