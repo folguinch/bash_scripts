cf5c696364ea8fd8611944b2821fa3afbb5b24c0
Fernando <f.olguin.ch@gmail.com>
2017-05-08 02:21:49 +0100

    List the notes in the Notes directory

--- notes.sh
+++ notes.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# A simple script to manage notes in the command line
 
 function list_dir {
+    # List the notes in the notes directory
     ls -lh $NOTES
 }
 
-
 NOTES=~/Notes
-list_dir
+
+if [ $1 = "-l" ]; then
+    list_dir
+fi
