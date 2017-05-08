48f18b91b573108d19ae627ac0a0923340e28f5c
Fernando <f.olguin.ch@gmail.com>
2017-05-08 17:07:25 +0100

    Improve command line options

--- notes.sh
+++ notes.sh
@@ -1,31 +1,44 @@
 #!/bin/bash -x
 # A simple script to manage notes in the command line
 
-# Fixed variables
+##### Constants
 NOTES=~/Notes
 EDITOR=vim
 
-# Functions
+##### Functions
 function list_dir {
     # List the notes in the notes directory
     ls -lh $NOTES
 }
 
+function get_filename {
+    if [ -n $1 ]; then
+        filename=$1
+    else
+        exit 1
+    fi
+}
 
-#Main
+##### Main
+note=""
+comment=""
 while [ "$1" != "" ]; do
     case $1 in
-        -l | --list )   list_dir
-                        ;;
-        -e | --edit )   shift
-                        $EDITOR "$NOTES/$1"
-                        ;;
-        -a | --append ) shift
-                        message=$1
-                        echo $message >> "$NOTES/$1"
-                        ;;
-        * )             exit 1
-                        ;;
+        -l | --list )       list_dir
+                            exit
+                            ;;
+        -e | --edit )       shift
+                            $EDITOR "$NOTES/$1"
+                            exit
+                            ;;
+        -a | --append )     shift
+                            note=$1
+                            ;;
+        -c | --comment )    shift
+                            comment=$1
+                            ;;
+        * )                 get_filename $1
+                            ;;
     esac
     shift
 done
